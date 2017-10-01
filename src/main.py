4c58356281d202fa6556e3117e11832fed05a051
Christoph Fricke <christoph@frickeonline.de>
2017-10-01 19:59:50 +0200

    Main now writes all primes number < 1000000 in a json file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 """Main file"""
 
 from prime import Prime
+import timeit
+import json
 
-for i in range(100):
-    if Prime.is_prime(i):
-        print(i)
+primeDict = {}
+primeId = 0
+for i in range(1000000):
+    start_time = timeit.default_timer()
+    result = Prime.is_prime(i)
+    end_time = timeit.default_timer()
+    if result:
+        primeDict[primeId] = {'prime': i, 'time': end_time - start_time}
+        primeId += 1
+with open('primes.json', 'w') as file:
+    json.dump(primeDict, file)
