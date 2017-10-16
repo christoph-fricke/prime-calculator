e658d6d5e89dc8c5f550998cfd43bd3e42092898
quannten24 <bendix.sonnenberg@gmx.de>
2017-10-16 21:17:10 +0200

    made it work with python3

--- src/main.py
+++ src/main.py
@@ -6,11 +6,11 @@ import subprocess
 primeDict = {}
 primeId = 0;
 primeStr = subprocess.check_output(['./a.out','1000000'])
-primeList = primeStr.split('\n')
+primeList = primeStr.split(b'\n')
 for element in primeList:
-	if element == "":
+	if element == b"":
 		continue
-	primeDict[primeId] = {'prime': element.split(',')[0], 'time': element.split(',')[1]}
+	primeDict[primeId] = {'prime': str(element.split(b',')[0]), 'time': str(element.split(b',')[1])}
 	primeId += 1
 with open('primes.json', 'w') as file:
     json.dump(primeDict, file)
