83e9bf32beb6b5187842198da2de41ca99b71ece
quannten24 <bendix.sonnenberg@gmx.de>
2017-10-16 20:54:45 +0200

    switched to using the c file as opposed to the python file

--- src/main.py
+++ src/main.py
@@ -1,17 +1,16 @@
 """Main file"""
 
-from prime import Prime
-import timeit
 import json
+import subprocess
 
 primeDict = {}
-primeId = 0
-for i in range(1000000):
-    start_time = timeit.default_timer()
-    result = Prime.is_prime(i)
-    end_time = timeit.default_timer()
-    if result:
-        primeDict[primeId] = {'prime': i, 'time': end_time - start_time}
-        primeId += 1
+primeId = 0;
+primeStr = subprocess.check_output(['./a.out','1000000'])
+primeList = primeStr.split('\n')
+for element in primeList:
+	if element == "":
+		continue
+	primeDict[primeId] = {'prime': element.split(',')[0], 'time': element.split(',')[1]}
+	primeId += 1
 with open('primes.json', 'w') as file:
     json.dump(primeDict, file)
